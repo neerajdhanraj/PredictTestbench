fbc04e8a6456819f15ab78fd6e452935240cc393
Neeraj Bokde <neerajdhanraj@gmail.com>
2016-07-02 00:40:05 +0530

    Add files via upload

---
+++ vignettes/my-vignette.R
@@ -0,0 +1,32 @@
+## ------------------------------------------------------------------------
+datax <- c(1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5,1:5)
+
+## ------------------------------------------------------------------------
+library(PredictTestbench)
+
+## ------------------------------------------------------------------------
+q <- prediction_errors(datax, nextVal = 10)
+q
+plot_predictions(q)
+
+## ------------------------------------------------------------------------
+#aa <- prediction_errors(dataIn= datax, nextVal = 10, MethodPath = "source('~/PredictTestbench/R/inter.R')", MethodName = "Proposed_Method")
+
+#aa
+#plot_predictions(aa)
+
+## ------------------------------------------------------------------------
+#bb <- prediction_append(existing_method = aa, dataIn= datax, nextVal = 10, MethodPath = "source('~/imputeTestbench/R/PSFimpute.R')", MethodName = "PSFimpute")
+
+#bb
+#plot_predictions(bb)
+
+## ------------------------------------------------------------------------
+# cc <- prediction_remove (existing_method = bb, index_number = 1)
+# cc
+# plot_predictions(cc)
+
+## ------------------------------------------------------------------------
+#a1 <- step_ahead_forecast(dataIn = datax, trainedData = 80, MethodPath = "source('~/imputeTestbench/R/PSFimpute.R')", errorParameter = 3, stepSize = 1)
+#a1
+
